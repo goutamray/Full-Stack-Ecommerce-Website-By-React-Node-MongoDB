09e6b3dd0d0d03e9dbffc5b5505596c78492ebc7
goutam <goutamr362@gmail.com>
2024-07-10 10:59:27 +0600

    header part fully done

--- src/components/header/navigation/Navigation.jsx
+++ src/components/header/navigation/Navigation.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 // react icons
-import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
+import { IoMenu } from "react-icons/io5";
 
 
 const Navigation = () => {
+  const [isOpenSideNav, setIsOpemSideNav ] = useState(true); 
   return (
     <>
         <nav className="header-bottom ">
@@ -13,11 +15,25 @@ const Navigation = () => {
              <div className="row my-custom-row">
                  <div className="col-sm-3">
                      <div className="category-tab">
-                       <button> 
+                       <button onClick={() => setIsOpemSideNav(!isOpenSideNav)}> 
                             <span className="menu"> <IoMenu /> </span>
                             <span> ALL CATEGORIES  </span>
                             <span className="angle"> <FaAngleDown /> </span>
                         </button>
+                        <div className={`sidebarnav ${isOpenSideNav === true ? "open" : ""}`}>
+                           <ul>
+                            <li> <Link href="#"> Men </Link></li>
+                            <li> <Link href="#"> Women </Link></li>
+                            <li> <Link href="#"> Beauty </Link></li>
+                            <li> <Link href="#"> Watches </Link></li>
+                            <li> <Link href="#"> Kids </Link></li>
+                            <li> <Link href="#"> Gifts </Link></li>
+                            <li> <Link href="#"> Beauty </Link></li>
+                            <li> <Link href="#"> Watches </Link></li>
+                            <li> <Link href="#"> Kids </Link></li>
+                            <li> <Link href="#"> Gifts </Link></li>
+                           </ul>
+                        </div>
                      </div>
                  </div>
                  <div className="col-sm-9">
@@ -28,16 +44,51 @@ const Navigation = () => {
                                Home 
                               <span> <FaAngleDown /> </span>
                            </Link>
+                           <div className="submenu shadow">
+                             <li> <Link href="/"> Home 1 </Link> </li>
+                             <li> <Link href="/"> Home 2 </Link> </li>
+                             <li> <Link href="/"> Home 3 </Link> </li>
+                            
+                         </div>
                         </li>
                         <li className="list-inline-item custom-icon ">
-                           <Link href="/">  shop 
+                           <Link href="/">  Men 
                            <span> <FaAngleDown /> </span>
                          </Link>
-
+                         <div className="submenu shadow">
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                         </div>
                         </li>
-                        <li className="list-inline-item"> <Link href="/">  Meats & Seafood</Link></li>
-                        <li className="list-inline-item"> <Link href="/"> backery </Link></li>
-                        <li className="list-inline-item"> <Link href="/">  Beverages </Link></li>
+                        <li className="list-inline-item"> 
+                          <Link href="/">  Women  <span> <FaAngleDown /> </span> </Link>
+                          <div className="submenu shadow">
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                         </div>
+                        </li>
+                        <li className="list-inline-item"> 
+                          <Link href="/"> Beauty  <span> <FaAngleDown /> </span>  </Link>
+                          <div className="submenu shadow">
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                             <li> <Link href="/">  Clothing </Link> </li>
+                             <li> <Link href="/">  FoodWear </Link> </li>
+                             <li> <Link href="/">  Watches </Link> </li>
+                         </div>
+                        </li>
+                        <li className="list-inline-item"> <Link href="/">  Watches </Link></li>
+                        <li className="list-inline-item"> <Link href="/">  Kids </Link></li>
+                        <li className="list-inline-item"> <Link href="/">  Gifts </Link></li>
                         <li className="list-inline-item"> <Link href="/">  blog </Link></li>
                         <li className="list-inline-item"> <Link href="/">  contact </Link></li>
                       </ul>

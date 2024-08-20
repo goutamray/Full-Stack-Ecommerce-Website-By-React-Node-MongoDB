2bacd138b7bccbba570862c75a4d35b354803f94
goutam <goutamr362@gmail.com>
2024-08-20 10:17:54 +0600

    slider updated & api added

--- src/components/homeBanner/HomeBanner.jsx
+++ src/components/homeBanner/HomeBanner.jsx
@@ -1,13 +1,17 @@
 
 import Slider from "react-slick";
 
+import { FaArrowRightLong } from "react-icons/fa6"; 
+
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 // images
-import slider1 from "../../assets/slider/slider1.jpg"
-import slider2 from "../../assets/slider/slider2.jpg"
+import slider1 from "../../assets/slider/slider1.webp";
+import slider2 from "../../assets/slider/slider2.webp";
+import slider3 from "../../assets/slider/slider3.webp";
+import { Link } from "react-router-dom";
 
 const HomeBanner = () => {
  
@@ -16,7 +20,7 @@ const HomeBanner = () => {
     arrows: true,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 7000,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
@@ -29,10 +33,58 @@ const HomeBanner = () => {
             <div className="col-sm-12">
                   <Slider {...settings}>
                     <div className="item">
-                        <img src={slider1} alt="slider1" height={350} className="rounded" />
+                        <img src={slider1} alt="slider1" height={400} className="rounded my-custom-size" />
+                       <div className="slide-content">
+                         <div className="offer-part">
+                           <h6> Exclusive offer </h6>
+                           <p> - 20% off </p>
+                         </div>
+                         <div className="main-content">
+                            <h1 className="entry-title"> A different kind of grocery store </h1>
+                            <p> Only this week. Don’t miss... </p>
+                          <div className="price">
+                            <p> from </p>
+                            <h1> $ 7.99 </h1>
+                          </div>
+                          <Link to="/shop"> Shop Now <FaArrowRightLong /></Link> 
+                         </div>
+                       </div>
                     </div>  
                     <div className="item">
-                        <img src={slider2} alt="slider2" height={350} className="rounded" />
+                        <img src={slider2} alt="slider2" height={400} className="rounded my-custom-size" />
+                        <div className="slide-content">
+                         <div className="offer-part">
+                           <h6> Exclusive offer </h6>
+                           <p> - 40% off </p>
+                         </div>
+                         <div className="main-content">
+                            <h1 className="entry-title"> Quality Freshness Guaranteed!  </h1>
+                            <p> Only this week. Don’t miss... </p>
+                          <div className="price">
+                            <p> from </p>
+                            <h1> $ 14.35 </h1>
+                          </div>
+                          <Link to="/shop"> Shop Now <FaArrowRightLong /></Link> 
+                         </div>
+                       </div>
+                    </div>  
+                    <div className="item">
+                        <img src={slider3} alt="slider2" height={400} className="rounded my-custom-size" />
+                        <div className="slide-content">
+                         <div className="offer-part">
+                           <h6> Exclusive offer </h6>
+                           <p> - 20% off </p>
+                         </div>
+                         <div className="main-content">
+                            <h1 className="entry-title"> Having the best quality products </h1>
+                            <p> Only this week. Don’t miss... </p>
+                          <div className="price">
+                            <p> from </p>
+                            <h1> $ 5.43 </h1>
+                          </div>
+                           <Link to="/shop"> Shop Now <FaArrowRightLong /></Link> 
+                         </div>
+                       </div>
                     </div>  
                 </Slider>
             </div>
---
+++ src/utils/api.js
@@ -0,0 +1,200 @@
+
+import axios from "axios"
+
+/**
+ *  fetch category data from api
+ * @param {*} url 
+ * @returns 
+ */
+export const fetchDataFromApi = async(url) => {
+   try {
+      const res = await axios.get("http://localhost:5050/api/v1/category"+url);
+      return res.data;
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }  
+}; 
+
+
+/**
+ * send category data to database from api 
+ * @param {*} url 
+ * @param {*} formData 
+ * @returns 
+ */
+export const postData = async(url, formData) => {
+   try {
+      const { res } = await axios.post("http://localhost:5050/api/v1/category"+ url, formData); 
+      return res;
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; // Re-throw the error for handling in the calling func
+   }
+}; 
+
+
+/**
+ * edit category data from api 
+ * @param {*} url 
+ * @param {*} updatedData 
+ * @returns 
+ */
+export const editData = async( url, updatedData ) => {
+  try {
+   const { res } = await axios.patch(`http://localhost:5050/api/v1/category${url}`, updatedData); 
+   return res;  
+  } catch (error) {
+   console.error('Error submitting form data:', error.message);
+   throw error; // Re-throw the error for handling in the calling func
+  }
+}; 
+
+
+/**
+ * delete category data from api 
+ * @param {*} id 
+ * @returns 
+ */
+export const deleteData = async( id ) => {
+   try {
+      const { res } = await axios.delete(`http://localhost:5050/api/v1/category${id}`); 
+      return res; 
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }
+}; 
+
+
+/**
+ * send product data to api 
+ * @param {*} url 
+ * @param {*} formData 
+ * @returns 
+ */
+export const productData = async (url, formData) => {
+   try {
+       const res = await axios.post(`http://localhost:5050/api/v1/product${url}`, formData);
+       return res.data; // Return the data property from the response
+   } catch (error) {
+       console.error('Error submitting form data:', error.message);
+       throw error; // Re-throw the error for handling in the calling function
+   }
+};
+
+
+/**
+ *  fetch product data from api
+ * @param {*} url 
+ * @returns 
+ */
+export const fetchProductFromApi = async(url) => {
+   try {
+      const res = await axios.get("http://localhost:5050/api/v1/product"+url);
+      return res.data;
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }
+    
+};
+
+
+/**
+ * delete product data from api 
+ * @param {*} id 
+ * @returns 
+ */
+export const deleteProductData = async( id ) => {
+   try {
+      const res = await axios.delete(`http://localhost:5050/api/v1/product${id}`); 
+      return res.data; 
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }
+}; 
+
+/**
+ * edit product data from api 
+ * @param {*} url 
+ * @param {*} updatedData 
+ * @returns 
+ */
+export const editProductData = async( url, updatedData ) => {
+   try {
+    const res = await axios.patch(`http://localhost:5050/api/v1/product${url}`, updatedData); 
+    return res.data;  
+   } catch (error) {
+    console.error('Error submitting form data:', error.message);
+    throw error; // Re-throw the error for handling in the calling func
+   }
+ }; 
+ 
+
+/**
+ * send brand data to database from api 
+ * @param {*} url 
+ * @param {*} formData 
+ * @returns 
+ */
+export const createBrandData = async(url, formData) => {
+   try {
+      const { res } = await axios.post("http://localhost:5050/api/v1/brand"+ url, formData); 
+      return res;
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }
+}; 
+
+
+/**
+ *  fetch data from api
+ * @param {*} url 
+ * @returns 
+ */
+export const fetchBrandDataFromApi = async(url) => {
+   try {
+      const res = await axios.get("http://localhost:5050/api/v1/brand"+url);
+      return res.data;
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }  
+}; 
+
+/**
+ * delete brand data from api 
+ * @param {*} id 
+ * @returns 
+ */
+export const deleteBrandData = async( id ) => {
+   try {
+      const res = await axios.delete(`http://localhost:5050/api/v1/brand${id}`); 
+      return res.data; 
+   } catch (error) {
+      console.error('Error submitting form data:', error.message);
+      throw error; 
+   }
+}; 
+
+
+/**
+ * edit brand data from api 
+ * @param {*} url 
+ * @param {*} updatedData 
+ * @returns 
+ */
+export const editBrandData = async( url, updatedData ) => {
+   try {
+    const res = await axios.patch(`http://localhost:5050/api/v1/brand${url}`, updatedData); 
+    return res.data;  
+   } catch (error) {
+    console.error('Error submitting form data:', error.message);
+    throw error; 
+   }
+ }; 
+
+
